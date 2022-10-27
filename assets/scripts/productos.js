b81500864a61dc0097b17d0b18e9d883418df2cc
juan-francoa <esteban_rlt159@hotmail.com>
2022-10-26 20:56:24 -0500

    implementacion filtros cruzados

--- assets/scripts/productos.js
+++ assets/scripts/productos.js
@@ -1,4 +1,7 @@
-const contCard = document.getElementById('cont_card')
+const contCardRe = document.getElementById('cont_card_remedios')
+const contCardJu = document.getElementById('cont_card_juguetes')
+const checkbox = document.getElementById("cont_checkbox")
+const search = document.getElementById("cont_search")
 const title = document.getElementById('title')
 
 async function api(){
@@ -9,13 +12,24 @@ async function api(){
     }
 
     let data = dataApi.response
-    data.filter(() => title.textContent.includes('Remedios')).filter(dato => dato.tipo === 'Medicamento').forEach(imprProducto)
-    data.filter(() => title.textContent.includes('Juguetes')).filter(dato => dato.tipo === 'Juguete').forEach(imprProducto)
+    imprimirCheckbox(checkbox)
+    if(contCardRe){
+        data.filter(dato => dato.tipo === 'Medicamento').forEach(val => imprProducto(val,contCardRe))
+        checkbox.addEventListener("change",value => filters("filterRadio", value.target.id, data, contCardRe).forEach(val => imprProducto(val,contCardRe)))
+        search.addEventListener("input", values => filters("filterSearch", values.target.value, data, contCardRe).forEach(val => imprProducto(val,contCardRe)))
+    }
+    if(contCardJu){
+        data.filter(dato => dato.tipo === 'Juguete').forEach(val => imprProducto(val,contCardJu))
+        checkbox.addEventListener("change",value => filters("filterRadio", value.target.id, data, contCardJu).forEach(val => imprProducto(val,contCardJu)))
+        search.addEventListener("input", values => filters("filterSearch", values.target.value, data, contCardJu).forEach(val => imprProducto(val,contCardJu)))
+    }
+    imprimirCheckbox(checkbox)
+    
     
 }
 api()
 
-function imprProducto(arreglo){
+function imprProducto(arreglo, contCard){
         contCard.innerHTML += 
                         `<div class="item">
                             <div class="card">
@@ -32,4 +46,71 @@ function imprProducto(arreglo){
                         </div>`
 }
 
+function filterRadio(value, data, main){
+    if(value == "flexRadioDefault1"){
+        main.innerHTML = "" 
+        return [...data.sort((a,b) => b.precio - a.precio)]
+    }
+    if(value == "flexRadioDefault2"){
+        main.innerHTML = "" 
+        return [...data.sort((a,b) => a.precio - b.precio)]
+    }
+}
+
+function filterSearch(values, data, main){
+    main.innerHTML = "" 
+    return data.filter(element => element.nombre.toLowerCase().includes(values.toLowerCase()))
+}
+
+function imprProducto(arreglo, contCard){
+        contCard.innerHTML += 
+                        `<div class="item">
+                            <div class="card">
+                                <p>$${arreglo.precio}</p>
+                                <img src="${arreglo.imagen}" alt="...">
+                                <div class="card_body">
+                                    <p class="card_text">${arreglo.nombre}</p>
+                                </div>
+                            </div>
+                            <div class="boton_card">
+                                <p>Comprar Ahora</p>
+                                <a href="#"><img src="../images/shop-car.png" alt=""></a>
+                            </div>
+                        </div>`
+}
+
+function imprimirCheckbox(main){
+    main.innerHTML =
+        `
+        <div class="form-check">
+        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+        <label class="form-check-label" for="flexRadioDefault1">
+            De mayor a menor precio
+        </label>
+        </div>
+        <div class="form-check">
+        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+        <label class="form-check-label" for="flexRadioDefault2">
+            De menor a mayor precio
+        </label>
+        </div>
+        `
+
+}
+
+let applied = {}
+function filters (fn, value, data, idImpresionCard){
+    let arrayBusqueda = data
+    applied[fn] = value
+    for(let name in applied){
+        console.log(name)
+        if( name === "filterRadio"){
+            arrayBusqueda = filterRadio(applied[name] , arrayBusqueda, idImpresionCard)
+        }
+        if( name === "filterSearch"){
+            arrayBusqueda = filterSearch(applied[name] , arrayBusqueda, idImpresionCard)
+        }
+    }
+    return arrayBusqueda
+}
 
